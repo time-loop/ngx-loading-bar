307b25d77bc54d8af489fe2a983dae633e4ef5db
Matt Lewis <matt@clickup.com>
2025-03-03 19:01:11 +0000

    build: upgrade to angular 18

--- packages/http-client/src/http.module.ts
+++ packages/http-client/src/http.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { LoadingBarInterceptor } from './loading-bar.interceptor';
 import { LoadingBarModule } from '@ngx-loading-bar/core';
 
-@NgModule({
-  imports: [HttpClientModule, LoadingBarModule],
-  exports: [HttpClientModule, LoadingBarModule],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: LoadingBarInterceptor,
-      multi: true,
-    },
-  ],
-})
+@NgModule({ exports: [HttpClientModule, LoadingBarModule], imports: [LoadingBarModule], providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: LoadingBarInterceptor,
+            multi: true,
+        },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class LoadingBarHttpClientModule {}
